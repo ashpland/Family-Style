5cf0e69be57d460946eef21c0dfefd472c5483bb
Andrew Shopland <hi@ashp.land>
2018-01-23 16:24:29 -0800

    Add Component View Controller

---
+++ Family Style/ViewControllers/FSTComponentViewController.swift
@@ -0,0 +1,72 @@
+//
+//  FSTComponentViewController.swift
+//  Family Style
+//
+//  Created by Andrew on 2018-01-23.
+//  Copyright © 2018 hearthedge. All rights reserved.
+//
+
+import UIKit
+
+class FSTComponentViewController: UIViewController {
+
+    var component: FSTComponent!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
+
+extension FSTComponentViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == self.component.ingredients.count {
+            print("Add item")
+        }
+        else {
+            print("Show item")
+        }
+        
+        
+        tableView.cellForRow(at: indexPath)?.isSelected = false
+        
+        
+    }
+    
+    
+}
+
+extension FSTComponentViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.component.ingredients.count + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.row == self.component.ingredients.count {
+            let addCell = tableView.dequeueReusableCell(withIdentifier: "IngredientCell", for: indexPath)
+            addCell.textLabel?.text = "+ Add Item"
+            addCell.accessoryType = UITableViewCellAccessoryType.none
+            return addCell
+        }
+            
+        else {
+            let ingredient = self.component.ingredients[indexPath.row]
+            let ingredientCell = tableView.dequeueReusableCell(withIdentifier: "IngredientCell", for: indexPath)
+            
+            ingredientCell.textLabel?.text = ingredient.name
+            
+            return ingredientCell
+        }
+        
+        
+    }
+    
+    
+    
+    
+}
